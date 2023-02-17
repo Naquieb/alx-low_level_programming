2ab982b39a4adcd3085c93ffba5feacabf881854
Naquieb <naquieb@gmail.com>
2023-02-17 06:15:53 -0800

    1-compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline"
+ * main - Entry
  * Return: Always 0
  */
-
 int main(void)
-
 {
-	puts'("\"Programming is like building a multilingual puzzle");
-
+	puts''i ('\"Programming is like building a multilingual puzzle");
 	return(0);
-
 }

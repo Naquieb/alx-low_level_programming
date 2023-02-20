3c43a280f305ad2cc5fa7f995cbd8f072c4163de
Naquieb <naquieb@gmail.com>
2023-02-20 06:40:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints "Programming is like building a multilingual
+ * puzzle, follow by a newline."
+ * return 0.
  */
+
 int main(void)
+
 {
-	puts''i ('\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return(0);
 }
